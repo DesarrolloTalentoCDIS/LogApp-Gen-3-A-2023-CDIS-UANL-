3cfe9723da51ea460d3d47e3ee020ea407607e62
LuisDiaz-ipsilon <99diazluisfernand@gmail.com>
2023-10-26 11:54:48 -0600

    8 Crear aplicación
    Clonar el repositorio de manera local, crear proyecto Angular, hacer commit y subir cambios.

---
+++ LogApp-Gen-3-A-2023-CDIS-UANL/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'LogApp-Gen-3-A-2023-CDIS-UANL';
+}
