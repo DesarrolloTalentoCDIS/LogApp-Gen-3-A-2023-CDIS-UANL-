e993f896c36c581914feb0905b630a3cc476ffe1
BionicHawk <angel.manzo.rosas@gmail.com>
2023-11-05 21:30:52 -0600

    Template y Rutas

--- LogApp-Gen-3-A-2023-CDIS-UANL/src/app/auth/auth-routing.module.ts
+++ LogApp-Gen-3-A-2023-CDIS-UANL/src/app/auth/auth-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '',
+    component: MainComponent, 
+    children: [
+      {path: 'login', component: LoginComponent},
+      {path: 'register', component: RegisterComponent},
+      {path: '**', redirectTo: 'login'},
+    ] 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AuthRoutingModule { }
+export class AuthRoutingModule {}
