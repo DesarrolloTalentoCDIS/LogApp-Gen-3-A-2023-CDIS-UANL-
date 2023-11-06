67493c67840e18a0a8a29580d1d356772ac9e488
Armando <armandoerj7@gmail.com>
2023-11-05 22:29:33 -0600

    19 Generar Service para el BackEnd
    Se generaron las carpetas de interfaces y servicios, generamos environments y generamos el servicio de autenticación

---
+++ LogApp-Gen-3-A-2023-CDIS-UANL/src/app/auth/services/auth.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { user } from '../Interfaces/us-interface';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private baseUrl : string = environment.baseUrl;
+  private _user! : user;
+
+  get user(){
+    return{...this._user}
+  }
+
+  constructor(private http: HttpClient) { }
+
+  register(usname : string, id : string, pass: string ){
+    const URL = `${this.baseUrl}/auth/new`;
+    const body = {usname, id, pass};
+  }
+
+  login(id : string, pass : string){
+    const URL = `${this.baseUrl}/auth`;
+    const body = {id, pass};
+  }
+
+  validateToken(){
+    
+  }
+}
---
+++ LogApp-Gen-3-A-2023-CDIS-UANL/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    baseUrl: 'https://user-authentication-server.herokuapp.com/api'
+};
